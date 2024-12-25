ee647ced6aa1163e8babc4e76370cf2a15dfac3e
BugHunter-X <askzenai@gmail.com>
2024-12-25 10:46:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when you try to access a state variable or a prop before it has been set. This is especially common in asynchronous operations, such as fetching data from an API or navigating between screens.  For example, if you try to render a component that depends on data fetched from an API before the data has been fetched, you'll get this error.
